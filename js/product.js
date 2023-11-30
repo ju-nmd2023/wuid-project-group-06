4f90a847a9276d99ae19d5345d4393babf9c0f56
vasilenagg <149382184+vasilenagg@users.noreply.github.com>
2023-11-30 17:49:40 +0100

    sliders struggle put as comments

--- js/product.js
+++ js/product.js
@@ -7,8 +7,10 @@ imagesProduct[2] = "img/product-page/white-dress-3.jpg";
 imagesProduct[3] = "img/product-page/white-dress-4.jpg";
 imagesProduct[4] = "img/product-page/white-dress.jpg";
 
-function startProduct () {
-document.getElementById("product-slideshow-container-mobile").style.backgroundImage = "url('" + imagesProduct[iProduct] + "')";
+function startProduct() {
+  document.getElementById(
+    "product-slideshow-container-mobile"
+  ).style.backgroundImage = "url('" + imagesProduct[iProduct] + "')";
 }
 // Function to move to the next image
 function nextImageProduct() {
@@ -20,7 +22,9 @@ function nextImageProduct() {
   }
 
   // Set the background image of the slideshow div
-  document.getElementById("product-slideshow-container-mobile").style.backgroundImage = "url('" + imagesProduct[iProduct] + "')";
+  document.getElementById(
+    "product-slideshow-container-mobile"
+  ).style.backgroundImage = "url('" + imagesProduct[iProduct] + "')";
 }
 
 // Function to move to the previous image
@@ -33,10 +37,10 @@ function prevImageProduct() {
   }
 
   // Set the background image of the slideshow div
-  document.getElementById("product-slideshow-container-mobile").style.backgroundImage = "url('" + imagesProduct[iProduct] + "')";
+  document.getElementById(
+    "product-slideshow-container-mobile"
+  ).style.backgroundImage = "url('" + imagesProduct[iProduct] + "')";
 }
-
-
 
 let iProductTablet = 0;
 let imagesProductTablet = [];
@@ -47,8 +51,11 @@ imagesProductTablet[2] = "img/product-page/white-dress-3.jpg";
 imagesProductTablet[3] = "img/product-page/white-dress-4.jpg";
 imagesProductTablet[4] = "img/product-page/white-dress.jpg";
 
-function startProductTablet () {
-document.getElementById("product-slideshow-container-tablet").style.backgroundImage = "url('" + imagesProductTablet[iProductTablet] + "')";
+function startProductTablet() {
+  document.getElementById(
+    "product-slideshow-container-tablet"
+  ).style.backgroundImage =
+    "url('" + imagesProductTablet[iProductTablet] + "')";
 }
 // Function to move to the next image
 function nextImageProductTablet() {
@@ -60,7 +67,10 @@ function nextImageProductTablet() {
   }
 
   // Set the background image of the slideshow div
-  document.getElementById("product-slideshow-container-tablet").style.backgroundImage = "url('" + imagesProductTablet[iProductTablet] + "')";
+  document.getElementById(
+    "product-slideshow-container-tablet"
+  ).style.backgroundImage =
+    "url('" + imagesProductTablet[iProductTablet] + "')";
 }
 
 // Function to move to the previous image
@@ -73,70 +83,89 @@ function prevImageProductTablet() {
   }
 
   // Set the background image of the slideshow div
-  document.getElementById("product-slideshow-container-tablet").style.backgroundImage = "url('" + imagesProductTablet[iProductTablet] + "')";
+  document.getElementById(
+    "product-slideshow-container-tablet"
+  ).style.backgroundImage =
+    "url('" + imagesProductTablet[iProductTablet] + "')";
 }
 
-
-
-
-
-
-window.onload = function (){
-    startProduct();
-    startProductTablet();
-}
-
+window.onload = function () {
+  startProduct();
+  startProductTablet();
+};
 
 function btnColor(button) {
-    // Remove 'active' class from all buttons
-    let buttons = document.querySelectorAll("#size-rectangle");
-    buttons.forEach(function(btn) {
-      btn.classList.remove("active");
-    });
+  // Remove 'active' class from all buttons
+  let buttons = document.querySelectorAll("#size-rectangle");
+  buttons.forEach(function (btn) {
+    btn.classList.remove("active");
+  });
 
-    // Add 'active' class to the clicked button
-    button.classList.add("active"); 
+  // Add 'active' class to the clicked button
+  button.classList.add("active");
+}
+
+function heartIcon() {
+  let icon = document.getElementById("heart");
+
+  if (icon.classList.contains("far")) {
+    icon.classList.remove("far");
+    icon.classList.add("fas");
+    document.getElementById("favourites-button").classList.add("active");
+  } else {
+    icon.classList.remove("fas");
+    icon.classList.add("far");
+    document.getElementById("favourites-button").classList.remove("active");
   }
+}
 
-  function heartIcon() {
-    let icon = document.getElementById("heart");
+function wishlistTablet() {
+  let wishlist = document.getElementById("wishlist-tablet");
 
-    if (icon.classList.contains("far")) {
-      icon.classList.remove("far");
-      icon.classList.add("fas");
-      document.getElementById("favourites-button").classList.add("active");
-    } else {
-      icon.classList.remove("fas");
-      icon.classList.add("far");
-      document.getElementById("favourites-button").classList.remove("active");
-    }
+  if (wishlist.classList.contains("far")) {
+    wishlist.classList.remove("far");
+    wishlist.classList.add("fas");
+  } else {
+    wishlist.classList.remove("fas");
+    wishlist.classList.add("far");
   }
+}
 
-  function wishlistTablet() {
-    let wishlist = document.getElementById("wishlist-tablet");
+function wishlist() {
+  let wishlist = document.getElementById("wishlist");
 
-    if (wishlist.classList.contains("far")) {
-      wishlist.classList.remove("far");
-      wishlist.classList.add("fas");
-    } else {
-        wishlist.classList.remove("fas");
-        wishlist.classList.add("far");
-    }
+  if (wishlist.classList.contains("far")) {
+    wishlist.classList.remove("far");
+    wishlist.classList.add("fas");
+  } else {
+    wishlist.classList.remove("fas");
+    wishlist.classList.add("far");
   }
+}
 
-  function wishlist() {
-    let wishlist = document.getElementById("wishlist");
+function toggleBorder() {
+  let chooseColour = document.getElementById("choose-colour");
+  chooseColour.classList.toggle("clicked");
+}
 
-    if (wishlist.classList.contains("far")) {
-      wishlist.classList.remove("far");
-      wishlist.classList.add("fas");
-    } else {
-        wishlist.classList.remove("fas");
-        wishlist.classList.add("far");
-    }
-  }
 
-  function toggleBorder() {
-    let chooseColour = document.getElementById('choose-colour');
-    chooseColour.classList.toggle('clicked');
-  }
+/*
+const sliderContainer = document.getElementById('slider-container');
+let currentIndex = 2; // Start with the duplicate set
+
+function showSlide(index) {
+  currentIndex = index;
+  sliderContainer.style.transform = `translateX(${-index * 25}%)`;
+}
+
+function nextSlide() {
+  currentIndex++;
+  showSlide(currentIndex);
+}
+
+function prevSlide() {
+  currentIndex--;
+  showSlide(currentIndex);
+}
+
+showSlide(currentIndex);
